7d984e5f10516d90b83d27a307282d2cb400375c
sky121016 <sky121016@naver.com>
2023-08-27 15:41:36 +0900

    Add Game Over, Complete 기능 추가,  CSS 완성

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ window.onload = function(){
     let board = document.getElementById("board");
     
 
-    
-
 
     // 문제 보드
     let numRow = document.getElementById("num-row");
@@ -64,13 +62,15 @@ window.onload = function(){
 
     // 문제 번호
     let num = 0;
-    let rb = document.querySelectorAll("#num-row > .numBlock > p");
-    
+    let correctBlockCnt = 0;
 
+    // row
+    let rb = document.querySelectorAll("#num-row > .numBlock > p");
     for(let r = 0; r<Data.size; r++){
         for(let c = 0; c<Data.size; c++){
             if(Data.pixel[r][c] == '1'){
                 num++;
+                correctBlockCnt++;              // 정답블록 개수 카운트
             }else{
                 if(num != '0'){
                     rb[r].innerHTML += num+" ";
@@ -84,7 +84,10 @@ window.onload = function(){
         num = 0;
     }
 
-    let cb = document.querySelectorAll("#num-col > .numBlock");
+    console.log(correctBlockCnt);
+
+    // col
+    let cb = document.querySelectorAll("#num-col > .numBlock > p");
     for(let c = 0; c<Data.size; c++){
         for(let r = 0; r<Data.size; r++){
             if(Data.pixel[r][c] == '1'){
@@ -108,35 +111,56 @@ window.onload = function(){
     let life = 5;
 
     let hearts = document.querySelectorAll(".heart > img");
-    
+
 
     blocks.forEach(b => {
         b.addEventListener("mousedown", function(){
             let row = b.dataset.row;
             let col = b.dataset.col;
             let visited = b.dataset.visited;
+
             
+
             if(Data.pixel[row][col] == '1' && visited === "false"){            // 맞음
-                this.style.backgroundColor = "#ffff00";
                 b.dataset.visited = "true";
+                this.classList.add("correct");
+
+
             }else if(Data.pixel[row][col] == '0' && visited === "false"){
                 this.classList.add("wrong");
                 life --;
                 hearts[life].style.display = "none";
                 b.dataset.visited = "true";
             }
+
             if(life == 0){
+                // Lose
                 GameOver();
             }
+
+            
+            let correctBlocks = document.querySelectorAll(".correct");
+            if(correctBlockCnt == correctBlocks.length){
+                // Win
+                GameComplete();
+            }
+        
         });
     });
 
 
 
-    function GameOver() {
-        let gameOver = document.getElementById("game-over");
-        gameOver.style.display = "block";
-    }
 
 
+}
+
+function GameComplete(){
+    let gameComplete = document.getElementById("game-complete");
+    gameComplete.style.display = "block";
+}
+
+
+function GameOver() {
+    let gameOver = document.getElementById("game-over");
+    gameOver.style.display = "block";
 }
